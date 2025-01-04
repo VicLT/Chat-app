6a9fb0fcfccf2fc9d6f778091b4f9e284b29aee4
Victor LT <victor_lamas@hotmail.com>
2025-01-04 19:33:05 +0100

    Zustand setup

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -9,9 +9,12 @@ import { toast } from 'sonner';
 import { apiClient } from '@/lib/api-client';
 import { LOGIN_ROUTE, SIGNUP_ROUTE } from '@/utils/constants';
 import { useNavigate } from 'react-router-dom';
+import { useAppStore } from '@/store';
+
 
 const Auth = () => {
     const navigate = useNavigate();
+    const { setUserInfo } = useAppStore();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -52,6 +55,7 @@ const Auth = () => {
                 { withCredentials: true }
             );
             if (response.data.user.id) {
+                setUserInfo(response.data.user);
                 if (response.data.user.profileSetup) {
                     navigate('/chat');
                 } else {
@@ -70,6 +74,7 @@ const Auth = () => {
                 { withCredentials: true }
             );
             if (response.status === 201) {
+                setUserInfo(response.data.user);
                 navigate('/profile');
             }
             console.log({ response });
