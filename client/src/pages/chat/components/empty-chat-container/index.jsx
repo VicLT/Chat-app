762ed3dc441a2d327da6fa38f8a3b28b913453ab
Victor LT <victor_lamas@hotmail.com>
2025-01-06 11:47:02 +0100

    Chat container

--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -13,7 +13,7 @@ const EmptyChatContainer = () => {
             <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
                 <h3 className="poppins-medium">
                     Hi
-                    <span className="text-purple-500">!</span>
+                    <span className="text-purple-500">! </span>
                     Welcome to
                     <span className="text-purple-500"> Syncronus </span>
                     Chat App
