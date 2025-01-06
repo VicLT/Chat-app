a438762148834dbcf1164126b16e89f5aa863ad4
Victor LT <victor_lamas@hotmail.com>
2025-01-06 17:16:55 +0100

    Contact Info

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -7,7 +7,7 @@ import EmptyChatContainer from "./components/empty-chat-container";
 import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
-    const { userInfo } = useAppStore();
+    const { userInfo, selectedChatType } = useAppStore();
     const navigate = useNavigate();
     useEffect(() => {
         if (!userInfo.profileSetup) {
@@ -15,12 +15,15 @@ const Chat = () => {
             navigate("/profile");
         }
     }, [userInfo, navigate]);
-    
+
     return (
         <div className="flex h-[100vh] text-white overflow-hidden">
             <ContactsContainer />
-            {/* <EmptyChatContainer /> */}
-            {/* <ChatContainer /> */}
+            {selectedChatType === undefined ? (
+                <EmptyChatContainer />
+            ) : (
+                <ChatContainer />
+            )}
         </div>
     );
 };
