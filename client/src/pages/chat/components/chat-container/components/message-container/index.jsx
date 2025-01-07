72fa7a355f4628b547b0c979fd7cf656f31c7b40
Victor LT <victor_lamas@hotmail.com>
2025-01-07 20:09:58 +0100

    Download/upload files UI finished

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -14,7 +14,9 @@ const MessageContainer = () => {
         selectedChatData,
         userInfo,
         selectedChatMessages,
-        setSelectedChatMessages
+        setSelectedChatMessages,
+        setFileDownloadProgress,
+        setIsDownloading
     } = useAppStore();
 
     const [showImage, setShowImage] = useState(false);
@@ -73,8 +75,15 @@ const MessageContainer = () => {
     };
 
     const downloadFile = async (url) => {
+        setIsDownloading(true);
+        setFileDownloadProgress(0);
         const response = await apiClient.get(`${HOST}/${url}`, {
-            responseType: "blob"
+            responseType: "blob",
+            onDownloadProgress: (progressEvent) => {
+                const { loaded, total } = progressEvent;
+                const percentCompleted = Math.round((loaded * 100) / total);
+                setFileDownloadProgress(percentCompleted);
+            }
         });
         const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement("a");
@@ -84,6 +93,8 @@ const MessageContainer = () => {
         link.click();
         link.remove();
         window.URL.revokeObjectURL(urlBlob);
+        setIsDownloading(false);
+        setFileDownloadProgress(0);
     };
 
     const renderDMMessages = (message) => (
