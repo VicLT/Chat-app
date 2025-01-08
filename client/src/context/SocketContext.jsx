c32422066220412bc59aef207c0c548370e0d473
Victor LT <victor_lamas@hotmail.com>
2025-01-08 17:51:03 +0100

    Finish: get channel messages (messages bug pending)

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -23,32 +23,43 @@ export const SocketProvider = ({ children }) => {
                 console.log("Connected to socket server.");
             });
 
-            const handleRecieveMessage = (message) => {
-                const { selectedChatData, selectedChatType, addMessage } =
-                    useAppStore.getState();
+            const handleReceiveMessage = (message) => {
+                const {
+                    selectedChatData,
+                    selectedChatType,
+                    addMessage,
+                    addContactsInDMContacts
+                } = useAppStore.getState();
                 if (
                     selectedChatType !== undefined &&
                     (selectedChatData._id === message.sender._id ||
                         selectedChatData._id === message.recipient._id)
                 ) {
-                    console.log("Recieved message: ", message);
+                    console.log("Received message: ", message);
                     addMessage(message);
                 }
+                addContactsInDMContacts(message);
             };
 
-            const handleRecieveChannelMessage = (message) => {
-                const { selectedChatData, selectedChatType, addMessage } =
-                    useAppStore.getState();
+            const handleReceiveChannelMessage = (message) => {
+                const {
+                    selectedChatData,
+                    selectedChatType,
+                    addMessage,
+                    addChannelInChannelList
+                } = useAppStore.getState();
 
                 if (
                     selectedChatType !== undefined &&
-                    selectedChatData._id === message.channelId) {
+                    selectedChatData._id === message.channelId
+                ) {
                     addMessage(message);
                 }
+                addChannelInChannelList(message);
             };
 
-            socket.current.on("recieveMessage", handleRecieveMessage);
-            socket.current.on("recieve-channel-message", handleRecieveChannelMessage);
+            socket.current.on("receiveMessage", handleReceiveMessage);
+            socket.current.on("receive-channel-message", handleReceiveChannelMessage);
 
             return () => {
                 socket.current.disconnect();
