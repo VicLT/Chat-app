5cc87a94cc54a2e63ba1b2c21e7c0a4143cda6b5
Victor LT <victor_lamas@hotmail.com>
2025-01-08 11:49:37 +0100

    Channel model, channel API and get user channels

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,4 +1,4 @@
-import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { GET_DM_CONTACTS_ROUTES, GET_USER_CHANNELS_ROUTE } from "@/utils/constants";
 import NewDM from "./components/new-dm";
 import ProfileInfo from "./components/profile-info";
 import { useEffect } from "react";
@@ -7,9 +7,8 @@ import { useAppStore } from "@/store";
 import ContactList from "@/components/contact-list";
 import CreateChannel from "./components/create-channel";
 
-
 const ContactsContainer = () => {
-    const { setDirectMessagesContacts, directMessagesContacts } = useAppStore();
+    const { setDirectMessagesContacts, directMessagesContacts, channels, setChannels } = useAppStore();
 
     useEffect(() => {
         const getContacts = async () => {
@@ -20,9 +19,18 @@ const ContactsContainer = () => {
                 setDirectMessagesContacts(response.data.contacts);
             }
         };
+        const getChannels = async () => {
+            const response = await apiClient.get(GET_USER_CHANNELS_ROUTE, {
+                withCredentials: true
+            });
+            if (response.data.channels) {
+                setChannels(response.data.channels);
+            }
+        };
 
         getContacts();
-    }, []);
+        getChannels();
+    }, [setChannels, setDirectMessagesContacts]);
 
     return (
         <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
@@ -42,6 +50,9 @@ const ContactsContainer = () => {
                 <div className="flex items-center justify-between pr-10">
                     <Title text="Channels" />
                     <CreateChannel />
+                </div>
+                <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+                    <ContactList contacts={channels} isChannel={true} />
                 </div>
             </div>
             <ProfileInfo />
